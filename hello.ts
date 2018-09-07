e84642564b71cafcc553d58bc3aac70033eb6098
freewind <nowindlee@gmail.com>
2018-09-07 17:11:51 +0800

    demo

--- hello.ts
+++ hello.ts
@@ -1,1 +1,14 @@
-console.log("Hello, Typescript");
+async function futureAdd(a: number, b: number) {
+    return new Promise((resolve) => {
+        console.log(`Promise: will add ${a} + ${b}`)
+        setTimeout(() => {
+            resolve(a + b)
+        }, 3000)
+    })
+}
+
+(async () => {
+        const result = await futureAdd(1, 2)
+        console.log(`result: ${result}`)
+    }
+)()
