98aecc39f6dfdc8d5bbe6cb195c7952dfde79be0
freewind <nowindlee@gmail.com>
2018-09-11 19:54:13 +0800

    demo

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-TypeScript Async Await Demo
-===========================
+TypeScript "await" in Filter Demo
+=================================
 
-在Typescript中async/await的用法跟在JavaScript基本一致。
+在JavaScript里没有办法直接在array的filter里使用`await`的。
+
+只能先在`map`里`await`，得到结果，再使用`Promise.all`拿到值，最后再使用`filter`。
 
 ```
 npm install
 npx ts-node hello.ts
+```
+
+输出：
+
+```
+[ { filename: '.gitignore', isFile: true, size: 22 },
+  { filename: 'README.md', isFile: true, size: 110 },
+  { filename: 'hello.ts', isFile: true, size: 480 },
+  { filename: 'package-lock.json', isFile: true, size: 3341 },
+  { filename: 'package.json', isFile: true, size: 163 },
+  { filename: 'tsconfig.json', isFile: true, size: 125 } ]
 ```
--- hello.ts
+++ hello.ts
@@ -1,14 +1,21 @@
-async function futureAdd(a: number, b: number) {
-    return new Promise((resolve) => {
-        console.log(`Promise: will add ${a} + ${b}`)
-        setTimeout(() => {
-            resolve(a + b)
-        }, 3000)
-    })
+import {promises as fs} from 'fs'
+
+async function fileInfo(file: string) {
+    const stat = await fs.stat(file)
+    return {
+        filename: file,
+        isFile: stat.isFile(),
+        size: stat.size
+    }
 }
 
-(async () => {
-        const result = await futureAdd(1, 2)
-        console.log(`result: ${result}`)
-    }
-)()
+async function demo() {
+    const files = await fs.readdir('.')
+    const allInfo = await Promise.all(files.map(async file => {
+        return await fileInfo(file)
+    }))
+    const filesInfo = allInfo.filter(info => info.isFile)
+    console.log(filesInfo)
+}
+
+demo()
